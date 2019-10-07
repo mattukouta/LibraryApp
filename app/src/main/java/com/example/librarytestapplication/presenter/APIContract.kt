fc4b12ac80c72dce8d98079a97083ab120002e34
Kouta Takamatsu <bb1016181@gmail.com>
2019-10-07 10:34:30 +0900

    mvp風に変更しました

---
+++ app/src/main/java/com/example/librarytestapplication/presenter/APIContract.kt
@@ -0,0 +1,21 @@
+package com.example.librarytestapplication.presenter
+
+import android.text.Editable
+import com.example.librarytestapplication.BasePresenter
+import com.example.librarytestapplication.BaseView
+import com.example.librarytestapplication.model.dataElement
+
+interface APIContract {
+    interface View : BaseView<Presenter>{
+
+        fun showAdapter()
+        fun showEmptyMessage()
+    }
+    interface Presenter : BasePresenter {
+
+        fun editTextCheck(editText: Editable)
+        fun getWikipediaRequest(keyword: String)
+        fun initWikipediaList()
+        fun addWikipediaList(result: dataElement)
+    }
+}
